3d066740a16dac797560623a0432592baecd44ae
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2025-04-06 16:56:05 +0200

    create assets images file

---
+++ lib/Core/Utils/assets.dart
@@ -0,0 +1,81 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAnneMarie 1
+  /// assets/images/anne-marie 1.png
+  static const String imagesAnneMarie1 = "assets/images/anne-marie 1.png";
+
+  /// Assets for imagesBack
+  /// assets/images/back.png
+  static const String imagesBack = "assets/images/back.png";
+
+  /// Assets for imagesDJ SNAKE
+  /// assets/images/DJ SNAKE.png
+  static const String imagesDJSNAKE = "assets/images/DJ SNAKE.png";
+
+  /// Assets for imagesDuaLipa 1
+  /// assets/images/Dua-lipa 1.png
+  static const String imagesDuaLipa1 = "assets/images/Dua-lipa 1.png";
+
+  /// Assets for imagesLeftAlignment
+  /// assets/images/left_alignment.png
+  static const String imagesLeftAlignment = "assets/images/left_alignment.png";
+
+  /// Assets for imagesLogo
+  /// assets/images/logo.png
+  static const String imagesLogo = "assets/images/logo.png";
+
+  /// Assets for imagesLoop
+  /// assets/images/loop.png
+  static const String imagesLoop = "assets/images/loop.png";
+
+  /// Assets for imagesLoopActive
+  /// assets/images/loop_active.png
+  static const String imagesLoopActive = "assets/images/loop_active.png";
+
+  /// Assets for imagesMarshmello 1
+  /// assets/images/marshmello 1.png
+  static const String imagesMarshmello1 = "assets/images/marshmello 1.png";
+
+  /// Assets for imagesNext
+  /// assets/images/next.png
+  static const String imagesNext = "assets/images/next.png";
+
+  /// Assets for imagesOnBoardingImage
+  /// assets/images/onBoardingImage.png
+  static const String imagesOnBoardingImage = "assets/images/onBoardingImage.png";
+
+  /// Assets for imagesPause
+  /// assets/images/pause.png
+  static const String imagesPause = "assets/images/pause.png";
+
+  /// Assets for imagesPlay
+  /// assets/images/play.png
+  static const String imagesPlay = "assets/images/play.png";
+
+  /// Assets for imagesQu1
+  /// assets/images/qu1.jpg
+  static const String imagesQu1 = "assets/images/qu1.jpg";
+
+  /// Assets for imagesQu2
+  /// assets/images/qu2.jpg
+  static const String imagesQu2 = "assets/images/qu2.jpg";
+
+  /// Assets for imagesQu3
+  /// assets/images/qu3.jpg
+  static const String imagesQu3 = "assets/images/qu3.jpg";
+
+  /// Assets for imagesRandom
+  /// assets/images/random.png
+  static const String imagesRandom = "assets/images/random.png";
+
+  /// Assets for imagesTaylorSwift 1
+  /// assets/images/taylor-swift 1.png
+  static const String imagesTaylorSwift1 = "assets/images/taylor-swift 1.png";
+
+  /// Assets for imagesTest
+  /// assets/images/test.png
+  static const String imagesTest = "assets/images/test.png";
+}
+
